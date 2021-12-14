686cbe82cb15865596da1dc3dc49d877ececdab7
Grace Chen <gchen@Graces-MacBook-Air.local>
2021-12-14 10:31:52 -0500

    fixed spacing

--- pipe_networking.c
+++ pipe_networking.c
@@ -10,30 +10,30 @@
   =========================*/
 int server_handshake(int *to_client) {
   int from_client = 0;
-	printf("SERVER CREATES WKP AND WAITS FOR CONNECTION:\n");
+  printf("SERVER CREATES WKP AND WAITS FOR CONNECTION:\n");
   mkfifo(WKP, 0644);
-	printf("created well known pipe\n");
+  printf("created well known pipe\n");
 
   from_client = open(WKP, O_RDONLY);
-	printf("opened well known pipe for reading\n");
+  printf("opened well known pipe for reading\n");
 
-	printf("SERVER RECIEVES MESSAGE FROM CLIENT:\n");
+  printf("SERVER RECIEVES MESSAGE FROM CLIENT:\n");
   char message[100];
   read(from_client, message, sizeof(message));
   message[strlen(message)] = 0;
-	printf("read well known pipe\n");
+  printf("read well known pipe\n");
 
-	printf("SERVER REMOVES WKP:\n");
+  printf("SERVER REMOVES WKP:\n");
   remove(WKP);
-	printf("removed well known pipe\n");
+  printf("removed well known pipe\n");
 
-	printf("SERVER SENDS MESSAGE TO CLIENT:\n");
+  printf("SERVER SENDS MESSAGE TO CLIENT:\n");
   *to_client = open(message, O_WRONLY);
-	printf("opened secret pipe %s\n", message);
+  printf("opened secret pipe %s\n", message);
   write(*to_client, ACK, sizeof(ACK));
-	printf("sent ACK\n");
+  printf("sent ACK\n");
 
-	printf("SERVER RECIEVES FINAL VERIFICATION MESSAGE:\n");
+  printf("SERVER RECIEVES FINAL VERIFICATION MESSAGE:\n");
   read(from_client, message, sizeof(message));
   printf("got final message\n");
 
@@ -51,32 +51,32 @@ int server_handshake(int *to_client) {
 int client_handshake(int *to_server) {
   int from_server = 0;
 
-	printf("CLIENT CREATES PIPE:\n");
-	char pid[100];
+  printf("CLIENT CREATES PIPE:\n");
+  char pid[100];
   sprintf(pid, "%d", getpid());
   from_server = mkfifo(pid, 0644);
-	printf("created secret pipe: %s\n", pid);
-	
-	printf("CLIENT SENDS MESSAGE TO SERVER:\n");
-  *to_server = open(WKP, O_WRONLY);
-	printf("opened well known pipe\n");
-  write(*to_server, pid, strlen(pid));
-	printf("sent unique secret pipe message\n");
+  printf("created secret pipe: %s\n", pid);
 
-	printf("CLIENT RECIEVES RESPONSE:\n");
+  printf("CLIENT SENDS MESSAGE TO SERVER:\n");
+  *to_server = open(WKP, O_WRONLY);
+  printf("opened well known pipe\n");
+  write(*to_server, pid, strlen(pid));
+  printf("sent unique secret pipe message\n");
+
+  printf("CLIENT RECIEVES RESPONSE:\n");
   from_server = open(pid, O_RDONLY);
-	printf("opened secret pipe\n");
+  printf("opened secret pipe\n");
   char message[100];
   read(from_server, message, sizeof(message));
-	printf("recieved ACK\n");
+  printf("recieved ACK\n");
 
-	printf("CLIENT REMOVES SECRET PIPE:\n");
+  printf("CLIENT REMOVES SECRET PIPE:\n");
   remove(pid);
-	printf("removed secret pipe\n");
+  printf("removed secret pipe\n");
 
-	printf("CLIENT SENDS FINAL VERIFICATION MESSAGE:\n");
+  printf("CLIENT SENDS FINAL VERIFICATION MESSAGE:\n");
   write(*to_server, ACK, sizeof(ACK));
-	printf("sent final ACK\n");
+  printf("sent final ACK\n");
 
   return from_server;
 }

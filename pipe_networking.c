bc1b9104f0bbc6b3641515b3300e2f3f4ffe296e
Grace Chen <73039310+chen-grace-p4@users.noreply.github.com>
2021-12-14 06:05:43 +0000

    added comments

--- pipe_networking.c
+++ pipe_networking.c
@@ -10,27 +10,27 @@
   =========================*/
 int server_handshake(int *to_client) {
   int from_client = 0;
+	//server creates WKP and waits for connection
   mkfifo(WKP, 0644);
 	printf("created well known pipe\n");
 
   from_client = open(WKP, O_RDONLY);
 	printf("opened well known pipe for reading\n");
-
+	//server recieves message from client
   char message[100];
   read(from_client, message, sizeof(message));
   message[strlen(message)] = 0;
 	printf("read well known pipe\n");
-
+	//server removes wkp
   remove(WKP);
 	printf("removed well known pipe\n");
 
-  printf("secret pipe: %s\n", message);
   *to_client = open(message, O_WRONLY);
-	printf("opened secret pipe\n");
-
+	printf("opened secret pipe %s\n", message);
+	//server sents message to client
   write(*to_client, ACK, sizeof(ACK));
 	printf("sent ACK\n");
-
+	//server recieves final verification message
   read(from_client, message, sizeof(message));
   printf("got final message\n");
 
@@ -48,28 +48,28 @@ int server_handshake(int *to_client) {
 int client_handshake(int *to_server) {
   int from_server = 0;
 
+	//client creates secret pipe
 	char pid[100];
   sprintf(pid, "%d", getpid());
-
   from_server = mkfifo(pid, 0644);
 	printf("created secret pipe: %s\n", pid);
   
   *to_server = open(WKP, O_WRONLY);
 	printf("opened well known pipe\n");
-
+	//client sends message to server
   write(*to_server, pid, strlen(pid));
-	printf("sent secret pipe\n");
+	printf("sent secret pipe message\n");
 
   from_server = open(pid, O_RDONLY);
 	printf("opened secret pipe\n");
-
+	//client recieves response
   char message[100];
   read(from_server, message, sizeof(message));
 	printf("recieved ACK\n");
-
+	//client removes secret pipe
   remove(pid);
 	printf("removed secret pipe\n");
-
+	//client sends final verification message
   write(*to_server, ACK, sizeof(ACK));
 	printf("sent final ACK\n");
 
